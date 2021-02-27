ab82370f396e8cc55c1b75d5f44c6f69e45c841b
fssroxx <fssroxx@gmail.com>
2021-02-27 15:48:16 +0300

    arrival/departure dates realized

--- src/components/segment/segment.js
+++ src/components/segment/segment.js
@@ -1,48 +1,62 @@
-import './segment.scss';
+import "./segment.scss";
+
+import moment from 'moment';
+import 'moment/locale/ru';
 
 const Segment = ({ segment }) => {
-    
-    const travelDurationHours = (segment.travelDuration / 60).toFixed(0)
-    const travelDurationMinutes = (segment.travelDuration % 60);
-    const showTravelDurationHours = (travelDurationHours === 0) ? null : (travelDurationHours + ' ' + 'ч')
-    const showTravelDurationMinutes = (travelDurationMinutes === 0) ? null : (travelDurationMinutes + ' ' + 'мин');
+  const travelDurationHours = (segment.travelDuration / 60).toFixed(0);
+  const travelDurationMinutes = segment.travelDuration % 60;
+  const showTravelDurationHours =
+    travelDurationHours === 0 ? null : travelDurationHours + " " + "ч";
+  const showTravelDurationMinutes =
+    travelDurationMinutes === 0 ? null : travelDurationMinutes + " " + "мин";
+    let departureTime = moment(segment.departureDate).format('H:mm');
+    let departureDate = moment(segment.departureDate).format('DD MMM ddd').toLowerCase();
+    let arrivalTime = moment(segment.arrivalDate).format('H:mm');
+    let arrivalDate = moment(segment.arrivalDate).format('DD MMM ddd').toLowerCase();
   return (
     <div className="flightTo">
       <div className="firstLine">
-        <span className="firstLine__from">{ segment.departureCity?.caption}</span>
         <span className="firstLine__from">
-          , { segment.departureAirport.caption}{" "}
+          {segment.departureCity?.caption}
+        </span>
+        <span className="firstLine__from">
+          , {segment.departureAirport.caption}{" "}
         </span>
         <span className="firstLine__uid-from">
-          ({ segment.departureAirport.uid})
+          ({segment.departureAirport.uid})
         </span>
         <i className="firstLine__arrow-icon">-></i>
-        <span className="firstLine__to">{ segment.arrivalCity?.caption}</span>
-        <span className="firstLine__to">, { segment.arrivalAirport.caption} </span>
-        <span className="firstLine__uid-to">({ segment.arrivalAirport.uid})</span>
+        <span className="firstLine__to">{segment.arrivalCity?.caption}</span>
+        <span className="firstLine__to">
+          , {segment.arrivalAirport.caption}{" "}
+        </span>
+        <span className="firstLine__uid-to">
+          ({segment.arrivalAirport.uid})
+        </span>
       </div>
       <div className="secondLine">
         <p className="flyFrom">
-          <span className="time">18:10</span>
-          <span className="date">19 aug, wen</span>
+          <span className="time">{departureTime}</span>
+          <span className="date">{departureDate}</span>
         </p>
 
-        <p className="totalTime">{ showTravelDurationHours }  {showTravelDurationMinutes}</p>
+        <p className="totalTime">
+          {showTravelDurationHours} {showTravelDurationMinutes}
+        </p>
         <p className="flyTo">
-          <span className="date">20 aug, tur</span>
-          <span className="time">19:45</span>
+          <span className="date">{arrivalDate}</span>
+          <span className="time">{arrivalTime}</span>
         </p>
       </div>
       <div className="transfer">
         <div className="transfer__line">
-            <div className="transfer__count">1 transfer</div>
+          <div className="transfer__count">1 transfer</div>
         </div>
-        
-        
       </div>
       <div className="companyName">
         <p className="companyName__content">
-          Рейс выполняет: { segment.airline.caption}
+          Рейс выполняет: {segment.airline.caption}
         </p>
       </div>
     </div>
